4a8918aac4db0f37ff092cdbf670f4d3df40319c
valeeriiiee <obleavalerie@gmail.com>
2024-06-15 18:38:16 +0800

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,6 +1,12 @@
 // import PropTypes from 'prop-types';
 // import css from './FriendListItem.module.css';
 
-export const FriendListItem = () => {
-  return <div>FriendListItem</div>;
+export const FriendListItem = ({ avatar, isOnline, name }) => {
+  return (
+    <li className="item">
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
 };
